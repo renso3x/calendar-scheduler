a353ff67b3e9ffbad4abdf4ca10c36cde46942a9
Romeo Enso <romeo.enso@viseo.com>
2019-10-17 13:42:45 +0800

    styling login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,14 +1,26 @@
-import React, { useState } from "react";
-import { Container, Button, Form, FormGroup, Label, Input } from "reactstrap";
-import { authenticate, setToken, getCurrentUser } from "../services/auth";
-import { Redirect } from "react-router-dom";
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 
-import "./Login.css";
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+import useStyles from './styles/login';
+import { authenticate, setToken, getCurrentUser } from '../services/auth';
+
+import './Login.css';
 
 const Login = ({ location }) => {
+  const classes = useStyles();
   const [state, setState] = useState({
-    email: "",
-    password: "",
+    email: '',
+    password: '',
     isSubmitting: false
   });
 
@@ -24,48 +36,74 @@ const Login = ({ location }) => {
     e.preventDefault();
     try {
       delete state.isSubmitting;
+      console.log(state);
       const response = await authenticate(state);
       if (response.status === 200) {
         setToken(response.data.token);
         const { state } = location;
-        window.location = state ? state.from.pathname : "/";
+        window.location = state ? state.from.pathname : '/';
       }
     } catch (e) {
       console.log(e);
     }
   };
 
-  const _goToRegister = () => (window.location.href = "/register");
   if (getCurrentUser()) return <Redirect to="/" />;
 
   return (
-    <Container>
-      <div className="form-login">
-        <Form>
-          <FormGroup>
-            <Label for="email">Email</Label>
-            <Input
-              type="email"
-              name="email"
-              placeholder="Email"
-              onChange={handleChangeText}
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="password">Password</Label>
-            <Input
-              type="password"
-              name="password"
-              onChange={handleChangeText}
-            />
-          </FormGroup>
-        </Form>
-        <button className="btn btn-primary btn-block" onClick={handleSubmit}>
-          Login
-        </button>
-        <button className="btn btn-secondary btn-block" onClick={_goToRegister}>
-          Register
-        </button>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            onChange={handleChangeText}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={handleChangeText}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={handleSubmit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item>
+              <Link href="/register" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
       </div>
     </Container>
   );
--- server/config/db.js
+++ server/config/db.js
@@ -52,42 +52,43 @@ const User = Conn.define('user', {
 User.hasMany(Schedule);
 Schedule.belongsTo(User);
 
-Conn.sync({ force: true })
-  .then(async () => {
-    console.log('Nice~ Database looks fine');
-    // const event = [
-    //   { start: 0, duration: 15, title: "Excercise" },
-    //   { start: 25, duration: 30, title: "Travel to work" },
-    //   { start: 30, duration: 30, title: "Plan day" },
-    //   { start: 60, duration: 15, title: "Review yesterday's commits" },
-    //   { start: 100, duration: 15, title: "Code review" },
-    //   { start: 180, duration: 90, title: "Have lunch with John" },
-    //   { start: 360, duration: 30, title: "Have lunch with John" },
-    //   { start: 370, duration: 45, title: "Have lunch with John" },
-    //   { start: 405, duration: 30, title: "Have lunch with John" }
-    // ];
-    // // //seeder
-    // const hashedPassword = await bcrypt.hash("password", 10);
-    // return User.create({
-    //   firstName: Faker.name.firstName(),
-    //   lastName: Faker.name.lastName(),
-    //   email: "admin@admin.com",
-    //   password: hashedPassword
-    // }).then(user => {
-    //   _.forEach(event, async evt => {
-    //     const { success, start, end } = newTimeParser(evt.start, evt.duration);
-    //     if (success) {
-    //       await user.createSchedule({
-    //         ...evt,
-    //         start,
-    //         end
-    //       });
-    //     }
-    //   });
-    // });
+Conn.sync({ force: true }).then(async () => {
+  console.log('Nice~ Database looks fine');
+  const event = [
+    { start: 0, duration: 15, title: 'Excercise' },
+    { start: 25, duration: 30, title: 'Travel to work' },
+    { start: 30, duration: 30, title: 'Plan day' },
+    { start: 60, duration: 15, title: "Review yesterday's commits" },
+    { start: 100, duration: 15, title: 'Code review' },
+    { start: 180, duration: 90, title: 'Have lunch with John' },
+    { start: 360, duration: 30, title: 'Have lunch with John' },
+    { start: 370, duration: 45, title: 'Have lunch with John' },
+    { start: 405, duration: 30, title: 'Have lunch with John' }
+  ];
+  // //seeder
+  const hashedPassword = await bcrypt.hash('password', 10);
+  return User.create({
+    firstName: Faker.name.firstName(),
+    lastName: Faker.name.lastName(),
+    email: 'admin@admin.com',
+    password: hashedPassword
   })
-  .catch(error => {
-    console.log(error, 'Something went wrong with the Database Update!');
-  });
+    .then(user => {
+      _.forEach(event, async evt => {
+        const { success, start, end } = newTimeParser(evt.start, evt.duration);
+        if (success) {
+          await user.createSchedule({
+            ...evt,
+            start,
+            end
+          });
+        }
+      });
+      // });
+    })
+    .catch(error => {
+      console.log(error, 'Something went wrong with the Database Update!');
+    });
+});
 
 module.exports = Conn;
