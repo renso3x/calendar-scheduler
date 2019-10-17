243aee08191e5287856537618799ba41125b70b6
Romeo Enso <romeo.enso@viseo.com>
2019-10-17 15:17:11 +0800

    production build

---
+++ public/precache-manifest.f32bb9048266edd3771baaab27340595.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2711a357f377fc2f5d5",
+    "url": "/static/css/main.5905a931.chunk.css"
+  },
+  {
+    "revision": "c2711a357f377fc2f5d5",
+    "url": "/static/js/main.c2711a35.chunk.js"
+  },
+  {
+    "revision": "426263410de6df1e5124",
+    "url": "/static/css/1.dd320315.chunk.css"
+  },
+  {
+    "revision": "426263410de6df1e5124",
+    "url": "/static/js/1.42626341.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "747f4b5f58fe0e9f20e00b445b5e5cde",
+    "url": "/index.html"
+  }
+];
